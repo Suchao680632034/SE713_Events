98804d319a455fbbe783c52809419e149c7b7213
Suchao Suwannapong <bombig@gmail.com>
2026-01-17 20:47:45 +0700

    Task 4 express and server creation

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import type { Request, Response } from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+
+app.get('/test', (req: Request, res: Response) => {
+    const id = req.query.id;   
+    const output = `id: ${id}`;
+    res.send(output);
+  })
+
