c27ddf99d2201e2ba2740de91834219bc88fc30b
Suchao Suwannapong <bombig@gmail.com>
2026-01-18 07:58:06 +0700

    Async-await Done

--- src/server.ts
+++ src/server.ts
@@ -20,19 +20,19 @@ app.get('/test', (req: Request, res: Response) => {
     res.send(output);
   })
 
- app.get("/events", (req, res) => {
+ app.get("/events", async (req, res) => {
     if (req.query.category) {
     const category = req.query.category;
-    const filteredEvents = getEventByCategory(category as string);
+    const filteredEvents = await getEventByCategory(category as string);
     res.json(filteredEvents);
     } else {
-    res.json(getAllEvents());
+    res.json(await getAllEvents());
     }
 });
 
- app.get("/events/:id", (req, res) => {
+ app.get("/events/:id", async(req, res) => {
     const id = parseInt(req.params.id);
-    const event = getEventById(id);
+    const event =await getEventById(id);
     if (event) {
     res.json(event);
     } else {
@@ -40,9 +40,9 @@ app.get('/test', (req: Request, res: Response) => {
     }
 });  
 
- app.post("/events", (req, res) => {
+ app.post("/events", async (req, res) => {
     const newEvent: Event = req.body;
-    addEvent(newEvent);
+    await addEvent(newEvent);
     res.json(newEvent);
 });
 
