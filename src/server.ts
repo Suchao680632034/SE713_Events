a9ca8ef73f193d6d35e0daf12a87b9c81f7f5025
Suchao Suwannapong <bombig@gmail.com>
2026-01-18 07:25:24 +0700

    Task01 Refractor to functions

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import type { Request, Response } from 'express'
+import { get } from 'node:http';
 const app = express()
 app.use(express.json());
 const port = 3000
@@ -88,16 +89,16 @@ app.get('/test', (req: Request, res: Response) => {
  app.get("/events", (req, res) => {
     if (req.query.category) {
     const category = req.query.category;
-    const filteredEvents = events.filter((event) => event.category === category);
+    const filteredEvents = getEventByCategory(category as string);
     res.json(filteredEvents);
     } else {
-    res.json(events);
+    res.json(getAllEvents());
     }
 });
 
  app.get("/events/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    const event = events.find((event) => event.id === id);
+    const event = getEventById(id);
     if (event) {
     res.json(event);
     } else {
@@ -107,10 +108,28 @@ app.get('/test', (req: Request, res: Response) => {
 
  app.post("/events", (req, res) => {
     const newEvent: Event = req.body;
-    newEvent.id = events.length + 1;
-    events.push(newEvent);
+    addEvent(newEvent);
     res.json(newEvent);
 });
 
+function getEventByCategory(category: string): Event[] {
+    const filteredEvents = events.filter((event) => event.category === category);
+    return filteredEvents;
+}
 
+function getAllEvents(): Event[] {
+    return events;
+}
+
+
+function getEventById(id: number): Event | undefined {
+    return events.find((event) => event.id === id);
+}   
+
+
+function addEvent(newEvent: Event): Event {
+    newEvent.id = events.length + 1; 
+    events.push(newEvent);
+    return newEvent;
+}
 
