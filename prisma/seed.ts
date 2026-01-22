cda29772f038586681050a20b07d240abebfbd0d
Suchao Suwannapong <bombig@gmail.com>
2026-01-22 18:01:46 +0700

    T4 made change in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,23 @@
 
 import {createEvents} from '../src/db/createEvent';
 import {prisma} from '../src/prisma'
-createEvents()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+
+const seedData = async () => {
+  await prisma.event.deleteMany();
+  await prisma.organizer.deleteMany();
+  await createEvents();
+};
+
+await seedData()
+
+
+
+
+// createEvents()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
